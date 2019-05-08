b85216f9d86892a34e1fa790021504ea4ef5a7c8
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-05-08 23:30:29 +0200

    Extract session[:current_email] to a current_email method

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,19 +2,19 @@ class TodosController < ApplicationController
   before_action :authenticate
 
   def index
-    @todos = Todo.where(email: session[:current_email])
+  @todos = Todo.where(email: current_email)
   end
   def new
     @todo = Todo.new
   end
 
   def create
-    @todo = Todo.create todo_params.merge(email: session[:current_email])
+    @todo = Todo.create todo_params.merge(email: current_email)
     redirect_to root_path
   end
 
   private
-
+  
   def todo_params
     params.require(:todo).permit(:task)
   end

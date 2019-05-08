2679beeab523fb63d22b655e170113dae636ef84
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-05-08 23:54:58 +0200

    Create user model instead of using email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     end
   end
   def signed_in?
-    current_email.present?
+    current_user.signed_in?
   end
 
   def current_email
@@ -15,4 +15,9 @@ class ApplicationController < ActionController::Base
   def sign_in_as email
     session[:current_email] = email
   end
+
+  def current_user
+    User.new(current_email)
+  end
+
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,14 +2,14 @@ class TodosController < ApplicationController
   before_action :authenticate
 
   def index
-  @todos = Todo.where(email: current_email)
+  @todos = current_user.todos
   end
   def new
     @todo = Todo.new
   end
 
   def create
-    @todo = Todo.create todo_params.merge(email: current_email)
+    current_user.todos.create todo_params
     redirect_to root_path
   end
 
